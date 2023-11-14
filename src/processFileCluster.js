7b67bc57434477fa1c7a99eda1c57fe9a15d1ea7
Julio Cavalin <julio.cavalin@gmail.com>
2023-11-14 22:39:23 +0000

    reafactor: Refactor process file cluster

--- src/processFileCluster.js
+++ src/processFileCluster.js
@@ -22,55 +22,72 @@ const WORKERS = process.env.CLUSTER_WORKERS || os.cpus().length;
 let finished = false;
 let workersCreated = [];
 
-const linesPerCluster = 100 * fileConfig.linesPerFile * fileConfig.filesPerDirectory;
-let from = 1;
-let to = linesPerCluster;
+const totalFileLines = 15793546;
+const directoriesPerWorker = Math.ceil(totalFileLines / fileConfig.linesPerFile / fileConfig.filesPerDirectory / WORKERS);
 
-const startWorker = (fromWorker, toWorker) => {
-    if (finished) {
-        console.log('[PRIMARY] Process finished, no more workers will be created');
-        return;
-    }
+let {from, to} = getSubdirectoryRange(1);
+for (let i = 0; i < directoriesPerWorker; i++) {
 
-    const workerLabel = `${fromWorker}_${toWorker}`;
-    if (workersCreated.includes(workerLabel)) {
-        console.log(`[PRIMARY] Worker ${workerLabel} already created`);
-        
-        from += linesPerCluster;
-        to += linesPerCluster; 
-        startWorker(fromWorker, toWorker);
-        return;
-    }
-
-    console.log(`[PRIMARY] Starting new worker ${workerLabel}`);
-    setupPrimary({
-        exec: __dirname + '/processFile.js',
-        args: [filePath, fromWorker, toWorker]
-    });
-    cluster.fork();
-
-    from += linesPerCluster;
-    to += linesPerCluster; 
-        
-    workersCreated.push(workerLabel);
-};
-
-for (let i = 0; i < WORKERS; i++) {
-    startWorker(from, to);
 }
 
-cluster.on('exit', (worker, code) => {
-    console.log(`[WORKER ${worker.process.spawnargs[3]}_${worker.process.spawnargs[4]}] died: code ${code}`);
 
-    if (code === 2 && !finished) {
-        startWorker(from, to);
-    } else {
-        finished = true;
-        console.log('[PRIMARY] Process finished');
-    }
-});
 
-console.timeEnd('[PRIMARY] Running time');
 
-const used = process.memoryUsage().heapUsed / 1024 / 1024;
-console.log(`[PRIMARY] The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+// console.log(linesPerWorker, fileConfig.linesPerFile, fileConfig.filesPerDirectory);
+// process.exit(0);
+
+// const linesPerCluster = 100 * fileConfig.linesPerFile * fileConfig.filesPerDirectory;
+// let from = 1;
+// let to = linesPerCluster;
+
+// const startWorker = (fromWorker, toWorker) => {
+//     if (finished) {
+//         console.log('[PRIMARY] Process finished, no more workers will be created');
+//         return;
+//     }
+
+//     const workerLabel = `${fromWorker}_${toWorker}`;
+//     if (workersCreated.includes(workerLabel)) {
+//         console.log(`[PRIMARY] Worker ${workerLabel} already created`);
+        
+//         from += linesPerCluster;
+//         to += linesPerCluster; 
+//         startWorker(fromWorker, toWorker);
+//         return;
+//     }
+
+//     console.log(`[PRIMARY] Starting new worker ${workerLabel}`);
+//     setupPrimary({
+//         exec: __dirname + '/processFile.js',
+//         args: [filePath, fromWorker, toWorker]
+//     });
+//     cluster.fork();
+
+//     from += linesPerCluster;
+//     to += linesPerCluster; 
+        
+//     workersCreated.push(workerLabel);
+// };
+
+// for (let i = 0; i < WORKERS; i++) {
+//     startWorker(from, to);
+// }
+
+// cluster.on('exit', (worker, code) => {
+//     console.log(`[WORKER ${worker.process.spawnargs[3]}_${worker.process.spawnargs[4]}] died: code ${code}`);
+
+//     if (code === 2 && !finished) {
+//         startWorker(from, to);
+//     } else {
+//         finished = true;
+//         console.log('[PRIMARY] Process finished');
+//     }
+// });
+
+// console.timeEnd('[PRIMARY] Running time');
+
+// const used = process.memoryUsage().heapUsed / 1024 / 1024;
+// console.log(`[PRIMARY] The script uses approximately ${Math.round(used * 100) / 100} MB`);
+
+process.exit(0);
